c5490f933636cba59b50bb106c203e6994c94333
wforyu <wforyu@users.noreply.github.com>
2026-07-02 13:58:25 +0700

    feat: dashboard - sejajarkan grafik pendapatan & stok dalam grid 2 kolom

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -3,7 +3,12 @@
 namespace App\Filament\Pages;
 
 use BackedEnum;
+use Filament\Schemas\Components\Component;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Livewire;
 use Filament\Support\Icons\Heroicon;
+use Filament\Widgets\WidgetConfiguration;
+use Illuminate\Support\Collection;
 
 class Dashboard extends \Filament\Pages\Dashboard
 {
@@ -16,5 +21,42 @@ class Dashboard extends \Filament\Pages\Dashboard
     public static function getNavigationItems(array $urlParameters = []): array
     {
         return array_map(fn ($item) => $item->extraAttributes(['title' => 'Ringkasan penjualan dan statistik toko']), parent::getNavigationItems($urlParameters));
+    }
+
+    public function getWidgetsContentComponent(): Component
+    {
+        return Grid::make($this->getColumns())
+            ->schema(fn (): array => $this->buildWidgetsSchema());
+    }
+
+    public function buildWidgetsSchema(): array
+    {
+        return Collection::make($this->getWidgets())
+            ->values()
+            ->filter(fn ($widget): bool => $this->normalizeWidgetClass($widget)::canView())
+            ->map(function ($widget, int $key): Component {
+                $widgetClass = $this->normalizeWidgetClass($widget);
+
+                $widgetData = $this->getWidgetData();
+
+                $properties = ($widget instanceof WidgetConfiguration)
+                    ? [...$widget->widget::getDefaultProperties(), ...$widget->getProperties()]
+                    : $widgetClass::getDefaultProperties();
+
+                if (property_exists($this, 'filters')) {
+                    $properties['pageFilters'] = $this->filters;
+                }
+
+                $ref = new \ReflectionClass($widgetClass);
+                $columnSpan = $ref->getProperty('columnSpan')->getDefaultValue();
+
+                return Livewire::make(
+                    $widgetClass,
+                    fn (): array => [...$widgetData, ...$properties],
+                )
+                    ->key("{$widgetClass}-{$key}")
+                    ->columnSpan($columnSpan);
+            })
+            ->all();
     }
 }
